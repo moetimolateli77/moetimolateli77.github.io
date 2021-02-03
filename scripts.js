b77ce7b63e12ac4d462da2cab107772e259513d3
moetimolateli77 <moetimolateli@gmail.com>
2021-02-03 20:09:10 +0200

    week 5

--- scripts.js
+++ scripts.js
@@ -1,22 +1,19 @@
 /* Input: Mass in Pounds
- * Processing: Convert the Mass from Pounds to Kilograms
+ * Processing: Convert the Mass from Pounds to Kilograms using lbs / 2.205
  * Output: The Mass converted to Kilograms
  */
 function poundsTokilograms() {
-	// Read mass in Pounds from the user.
-	let text = document.getElementById("pounds").value; // input
+	// Input: Read mass in Pounds from the user.
+	let lbs = parseFloat(document.getElementById("pounds").value);
 
-	// Convert what the user typed from text into a number.
-	let lbs = parseFloat(text); // processing
+	// Processing: Convert Pounds into Kilograms.
+	let kg = lbs / 2.205;
 
-	// Convert Pounds into Kilograms.
-	let kg = lbs / 2.205; // processing
+	// Processing: Round the mass to 1 digit after the decimal.
+	let digits = 1;
+	let multiplier = Math.pow(10, digits);
+	kg = Math.round(kg * multiplier) / multiplier;
 
-	// Round the mass to 1 digit after the decimal.
-	let digits = 1; // processing
-	let multiplier = Math.pow(10, digits); // processing
-	kg = Math.round(kg * multiplier) / multiplier; // processing
-
-	// Display the mass in Kilograms to the user.
-	document.getElementById("output").innerHTML = kg + " " + "Kilograms"; // output
+	// Output: Display the mass in Kilograms to the user.
+	document.getElementById("output").innerHTML = kg + " " + "Kilograms"; 
 }
